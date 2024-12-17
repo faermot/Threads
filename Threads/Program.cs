1f1fca7a182673e645f40af859e25a192d4f0bb1
Ilya Karasev <97747062+faermot@users.noreply.github.com>
2024-12-17 23:23:20 +0300

    Добавьте файлы проекта.

---
+++ Threads/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Threads
+{
+    internal class Program
+    {
+        public delegate void ParameterizedThreadStart(object obj);
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.Write("Выберите задание: ");
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Задание №1");
+
+                            Thread thread = new Thread(PrintMessage);
+
+                            thread.Start();
+
+                            thread.Join();
+
+                            Console.WriteLine("Main thread finished");
+
+                            Console.ReadKey();
+                        }
+                        break;
+
+                    case "2":
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Задание №2");
+
+                            Thread threadOne = new Thread(PrintNumbersFromOneToFive);
+                            Thread threadTwo = new Thread(PrintNumbersFromOneToFive);
+
+                            threadOne.Start();
+                            threadTwo.Start();
+
+                            threadOne.Join();
+                            threadTwo.Join();
+
+                            Console.WriteLine("Пупупупу");
+
+                            Console.ReadKey();
+                        }
+                        break;
+
+                    case "3":
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Задание №3");
+
+
+
+                            Thread thread = new Thread(ThreadFromParameters);
+                            thread.Start("Hello");
+
+
+                            Console.ReadKey();
+                        }
+                        break;
+
+                    case "4":
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Задание №4");
+
+                            for (int i = 1; i < 6; i++)
+                            {
+                                Console.WriteLine(DateTime.Now.ToString("HH:mm:ss"));
+                                Thread.Sleep(1000);
+                            }
+
+
+                            Console.ReadKey();
+                        }
+                        break;
+
+                    case "5":
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Задание №5");
+
+
+                        }
+                        break;
+
+                    default:
+                        Console.WriteLine("Выберите корректное задание!");
+                        Thread.Sleep(3000);
+                        break;
+                }
+            }
+        }
+
+        static void PrintMessage()
+        {
+            Console.WriteLine("Hello from thread!");
+        }
+
+        static void PrintNumbersFromOneToFive()
+        {
+            for (int i = 1; i < 6; i++)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(1000);
+            }
+        }
+
+        static void ThreadFromParameters(object message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ Threads/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Threads")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Threads")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("93eb6ef1-489b-4580-818e-40d61fb6d319")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
